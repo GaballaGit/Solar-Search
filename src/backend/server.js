67af5b6bae2d093a02aca3c177800c9eb5b1abf9
Kab00mer <mryvkin@student.cccd.edu>
2025-04-12 23:32:26 -0700

    complete backend server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
 const app = express();
 
 app.get ('/planet', async (req, res) => {
@@ -12,18 +10,31 @@ app.get ('/planet', async (req, res) => {
     }
     
     try {
-        const response = await axios.get('https://api.api-ninjas.com/v1/planets', 
-            {
-                params: {name: name},
-                headers: {
-                    'X-Api-Key': process.env.API_KEY
-                }
-            }
-        );
+        const response = await axios.get(`https://api.le-systeme-solaire.net/rest/bodies/${name}`);
+        const data = response.data;
 
-        res.json(response.data);
+        const planet = {
+            name: data.englishName, //string
+            moons: data.moons.length, //int
+            mass: data.mass.massValue * 10 ** data.mass.massExponent, //kg
+            radius: data.meanRadius, //km
+            semi_major_axis: data.semimajorAxis, //km
+            gravity: data.gravity, // m/s^2
+            density: data.density,  // g/cm^3
+            escape_velocity: data.escape, //m/s
+            orbital_period: data.sideralOrbit, //hours
+            rotation_period: data.sideralRotation //hours
+        };
+
+        res.json(planet);
 
     } catch (error) {
         res.status(500).json({ error: 'Failed to Fetch Planet' });
     }
+    
+});
+
+const port = 8000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
