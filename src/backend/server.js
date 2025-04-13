2d63870338e99b02dc80f8f92424736f1b7d09b7
Kab00mer <mryvkin@student.cccd.edu>
2025-04-12 20:27:46 -0700

    prototype server

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,26 +3,27 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const app = express();
-const port = 8000;
 
-var name = "Venus";
-
-app.get('/', async (req, res) => {
-    const response = await axios.get('https://api.api-ninjas.com/v1/planets', 
-        {
-            params: {name: name},
-            headers: {
-                'X-Api-Key': process.env.API_KEY
+app.get ('/planet', async (req, res) => {
+    const name = req.query.name;
+    if (!name) {
+        return res.status(400).json({ error: 'Missing Planet Name' });
+    }
+    
+    try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/planets', 
+            {
+                params: {name: name},
+                headers: {
+                    'X-Api-Key': process.env.API_KEY
+                }
             }
-        }
-    );
+        );
 
-    console.log(JSON.stringify(response.data, 2, null));
-    res.json(response.data);
-});
+        res.json(response.data);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to Fetch Planet' });
+    }
 });
