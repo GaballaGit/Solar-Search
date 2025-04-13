876fb4a25ef0ec53625a709f2740b7d3da168ae9
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-04-12 20:11:02 -0700

    Star background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,37 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<style>
+  .stars {
+    background: black url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/stars.png) repeat;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: block;
+    z-index: 0;
+}
+
+  .twinkle {
+      hight: 10000px;
+      width: 100%;
+
+      background: transparent url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/twinkling.png") repeat;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      z-index: 2;
+
+  }
+</style>
+
+<body>
+  /* Body
+</body>
+
+<div class="stars"></div>
+<div class="twinkle"></div>
+
+
